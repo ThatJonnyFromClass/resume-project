bff4783c5699fa52d8707ecd83c055a3e6a44cb0
ThatJonnyFromClass <jonathansimmons@oru.edu>
2025-09-23 19:48:33 -0500

    Update resume.js
    fixed some bugs i made (i think)

--- resume.js
+++ resume.js
@@ -6,17 +6,15 @@ let myName = "Jonny";
 let title = "Student";
 let experience = "College Student, Highschool Student, and Middleschool Student";
 let education = "Oral Roberts University";
-let skills = ["Airsoft", "Gaming", "Persistence", "Academics"];
-let skillsList = document.createElement("ul");
 
+let skills = ["Airsoft", "Gaming", "Persistence", "Academics"];
+
+let skillsList = document.createElement("ul");
 for (let i = 0; i < skills.length; i++) {
   let li = document.createElement("li");
   li.textContent = skills[i];
   skillsList.appendChild(li);
 }
-document.body.appendChild(skillsList);
-
-<button onclick="showFunFact()">Show Fun Fact</button>
 
 function showFunFact() {
   alert("Fun Fact: I enjoy airsoft.");
@@ -27,5 +25,4 @@ document.getElementById("my-name").textContent = myName;
 document.getElementById("title").textContent = title;
 document.getElementById("experience").textContent = experience;
 document.getElementById("education").textContent = education;
-
-console.log("Hello, my name is " + myName);
+document.getElementById("skills").appendChild(skillsList);
