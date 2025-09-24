43171c00653b4598da349151ad0325646059866f
ThatJonnyFromClass <jonathansimmons@oru.edu>
2025-09-23 19:58:17 -0500

    Update resume.js
    another attempt

--- resume.js
+++ resume.js
@@ -2,7 +2,7 @@
 /* this won't run (multiline)
 */ 
 // Define variables
-let myName = "Jonny";
+let name = "Jonny";
 let title = "Student";
 let experience = "College Student, Highschool Student, and Middleschool Student";
 let education = "Oral Roberts University";
@@ -21,7 +21,7 @@ function showFunFact() {
 }
 
 // Connect Variable to the HTML (ligaments)
-document.getElementById("my-name").textContent = myName;
+document.getElementById("name").textContent = name;
 document.getElementById("title").textContent = title;
 document.getElementById("experience").textContent = experience;
 document.getElementById("education").textContent = education;
